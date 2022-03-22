9d05b531df988218b28d4c07a6c69d3e22260caf
Javier Villacorta <javier.vilgar@educa.jcyl.es>
2022-03-22 12:34:21 +0100

    ver0.0.01

--- app/src/main/java/com/example/puzzle2/Puzzle.java
+++ app/src/main/java/com/example/puzzle2/Puzzle.java
@@ -5,7 +5,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.Random;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
 
 public class Puzzle extends Activity implements View.OnClickListener {
 
@@ -40,16 +45,104 @@ public class Puzzle extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        //Para 3x3
+        switch (v.getId()){
+            case R.id.button1:
+                if (boton2.getText() == "0"){
+                    boton2.setText(boton1.getText());
+                    boton1.setText("0");
+                }else if (boton4.getText() == "0"){
+                    boton4.setText(boton1.getText());
+                    boton1.setText("0");
+                }
+                break;
 
+            case R.id.button2:
+                if (boton1.getText() == "0"){
+                    boton1.setText(boton2.getText());
+                    boton2.setText("0");
+                }else if (boton3.getText() == "0"){
+                    boton3.setText(boton2.getText());
+                    boton2.setText("0");
+                }else if (boton5.getText() == "0"){
+                    boton5.setText(boton2.getText());
+                    boton2.setText("0");
+                }
+                break;
+
+            case R.id.button3:
+                if (boton2.getText() == "0"){
+                    boton2.setText(boton3.getText());
+                    boton3.setText("0");
+                }else if (boton6.getText() == "0"){
+                    boton6.setText(boton3.getText());
+                    boton3.setText("0");
+                }
+                break;
+
+            case R.id.button4:
+                if (boton1.getText() == "0"){
+                    boton1.setText(boton4.getText());
+                    boton4.setText("0");
+                }else if (boton5.getText() == "0"){
+                    boton5.setText(boton4.getText());
+                    boton4.setText("0");
+                }else if (boton7.getText() == "0"){
+                    boton7.setText(boton4.getText());
+                    boton4.setText("0");
+                }
+                break;
+
+            case R.id.button5:
+                if (boton2.getText() == "0"){
+                    boton2.setText(boton5.getText());
+                    boton5.setText("0");
+                }else if (boton4.getText() == "0"){
+                    boton4.setText(boton5.getText());
+                    boton5.setText("0");
+                }else if (boton6.getText() == "0"){
+                    boton6.setText(boton5.getText());
+                    boton5.setText("0");
+                }else if (boton8.getText() == "0"){
+                    boton8.setText(boton5.getText());
+                    boton5.setText("0");
+                }
+                break;
+
+            case R.id.button6:
+                if (boton3.getText() == "0"){
+                    boton3.setText(boton6.getText());
+                    boton6.setText("0");
+                }else if (boton4.getText() == "0"){
+                    boton4.setText(boton6.getText());
+                    boton6.setText("0");
+                }else if (boton9.getText() == "0"){
+                    boton9.setText(boton6.getText());
+                    boton6.setText("0");
+                }
+                break;
+        }
     }
 
     public void crea_aleatoriamente(){
-        int [] combinacion = new int [9];
-        Random random = new Random();
 
-        for (int i = 0; i<9; i++){
-            combinacion [i] = random.nextInt(8);
-            System.out.println(combinacion [i]);
-        }
+        String [] c3x3 = {"0", "1", "2", "3", "4", "5", "6", "7", "8"};
+        List<String> C3x3 = Arrays.asList(c3x3);
+
+        Collections.shuffle(C3x3);
+        System.out.println(C3x3);
+        System.out.println(C3x3.get(2));
+
+        boton1.setText(C3x3.get(0));
+        boton2.setText(C3x3.get(1));
+        boton3.setText(C3x3.get(2));
+        boton4.setText(C3x3.get(3));
+        boton5.setText(C3x3.get(4));
+        boton6.setText(C3x3.get(5));
+        boton7.setText(C3x3.get(6));
+        boton8.setText(C3x3.get(7));
+        boton9.setText(C3x3.get(8));
+
     }
+
 }
